b6b0624e1d8ecac708f7046a53de261d67cb8bb1
guilhermejcgois <guijocargo@gmail.com>
2019-11-04 22:10:15 -0200

    Configure dotenv

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
+
+require('dotenv').config();
+
 export default {
     uri: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_URL}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 };
