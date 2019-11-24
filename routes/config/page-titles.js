dee94411cd686a156e39cd15cd6518808f13fd8c
guilhermejcgois <guijocargo@gmail.com>
2019-11-24 19:28:33 -0200

    Fix double title in bin rrequest page

--- routes/config/page-titles.js
+++ routes/config/page-titles.js
@@ -1,6 +1,6 @@
 export default {
     BINS: 'Minhas Bins',
-    BIN_REQUEST: 'Cadastar nova Bin',
+    BIN_REQUEST: 'Configure seu ponto',
     DASHBOARD: 'Dashboard',
     FORGOT_PASS: 'Esqueci minha senha',
     GATHERING: 'Minhas Coletas',
