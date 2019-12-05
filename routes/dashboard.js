1dedc97873e66d90aa6a25e116567da3e59f8508
guilhermejcgois <guijocargo@gmail.com>
2019-12-05 02:43:31 -0200

    Add chart in dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -22,6 +22,49 @@ const dashCb = (renderFn) => (req, res) => {
         renderFn(req, res, org);
     });
 };
+
+const allOrdersForOrgCb = successCb => (err, orders) => {
+    if (err) {
+        res.render('login', err);
+
+        return;
+    }
+
+    successCb(orders);
+};
+
+const findsPlacesAndRender = cb => rawBins => {
+    const firstPromise = Promise.resolve({ bins: {}, places: {} });
+    const placeIsCached = (state, placeId) => !!state.places[placeId];
+    const reduceBins = async (previousPromise, rawBin) => {
+        const state = await previousPromise;
+        if (!rawBin.place) { return Promise.resolve(state); }
+
+        if (placeIsCached(state, rawBin.place)) {
+            state.bins[rawBin._id] = {
+                ...rawBin._doc,
+                place: state.places[rawBin.place]
+            };
+            return Promise.resolve(state);
+        }
+
+        return Places.findById(rawBin.place).then(place => {
+            if (!place) { return state; }
+
+            state.places[place._id] = place;
+            state.bins[rawBin._id] = {
+                ...rawBin._doc,
+                place
+            };
+
+            return state;
+        });
+    };
+    rawBins.filter(rb => !!rb)
+        .reduce(reduceBins, firstPromise)
+        .then(cb);
+};
+
 const renderWithPage = (req, res, org) => {
     const page = req.params.page || 'dashboard';
     const title = PAGE_TITLES[page.toUpperCase()];
@@ -35,48 +78,6 @@ const renderWithPage = (req, res, org) => {
             lang,
             ...opts
         });
-    
-    const allOrdersForOrgCb = successCb => (err, orders) => {
-        if (err) {
-            res.render('login', err);
-
-            return;
-        }
-
-        successCb(orders);
-    };
-
-    const findsPlacesAndRender = cb => rawBins => {
-        const firstPromise = Promise.resolve({ bins: {}, places: {} });
-        const placeIsCached = (state, placeId) => !!state.places[placeId];
-        const reduceBins = async (previousPromise, rawBin) => {
-            const state = await previousPromise;
-            if (!rawBin.place) { return Promise.resolve(state); }
-
-            if (placeIsCached(state, rawBin.place)) {
-                state.bins[rawBin._id] = {
-                    ...rawBin._doc,
-                    place: state.places[rawBin.place]
-                };
-                return Promise.resolve(state);
-            }
-
-            return Places.findById(rawBin.place).then(place => {
-                if (!place) { return state; }
-
-                state.places[place._id] = place;
-                state.bins[rawBin._id] = {
-                    ...rawBin._doc,
-                    place
-                };
-
-                return state;
-            });
-        };
-        rawBins.filter(rb => !!rb)
-            .reduce(reduceBins, firstPromise)
-            .then(cb);
-    };
 
     switch (page) {
         case 'dashboard':
@@ -102,12 +103,13 @@ const renderWithPage = (req, res, org) => {
                                 break;
                         }
                     });
-                    
+
                     render({
                         activeBins: activeBins.length,
                         gathering: {
                             emptyBins,
-                            fullBins
+                            fullBins,
+                            total: emptyBins + fullBins 
                         },
                         places: Object.values(
                             activeBins.map(bin => bin.place).reduce((acc, place) => ({
@@ -149,6 +151,50 @@ const renderNewBin = (req, res, org) => {
     });
 }
 router.get('/', configAuth.ensureAuthenticated, dashCb(renderWithPage));
+router.get('/data', configAuth.ensureAuthenticated, (req, res) =>  {
+    Orders.find({ organization: req.user.organization }, allOrdersForOrgCb(orders => {
+        const binsPromises = orders.map(order => Bins.findById(order.bin));
+        Promise.all(binsPromises).then(findsPlacesAndRender(response => {
+            const bins = Object.values(response.bins || {});
+            let activeBins = [];
+            let emptyBins = 0;
+            let fullBins = 0;
+
+            bins.forEach(bin => {
+                if (bin.gathering_status != 'STATUS_INACTIVE') {
+                    activeBins.push(bin);
+                }
+
+                switch (bin.gathering_status) {
+                    case 'STATUS_ACTIVE':
+                        emptyBins++;
+                        break;
+                    case 'STATUS_GATHERING':
+                        fullBins++;
+                        break;
+                }
+            });
+
+            res.send({
+                activeBins: activeBins.length,
+                gathering: {
+                    emptyBins,
+                    fullBins,
+                    total: emptyBins + fullBins
+                },
+                places: Object.values(
+                    activeBins.map(bin => bin.place).reduce((acc, place) => ({
+                        ...acc,
+                        [`${place.latitude}:${place.longitude}`]: {
+                            lat: place.latitude,
+                            lng: place.longitude
+                        }
+                    }), {})
+                )
+            });
+        }));
+    }));
+});
 router.get('/:page', configAuth.ensureAuthenticated, dashCb(renderWithPage));
 router.get('/bins/cadastrar', configAuth.ensureAuthenticated, dashCb(renderNewBin));
 
